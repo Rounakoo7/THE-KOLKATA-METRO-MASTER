f7eadf3bcac454868ea6a8cf1aef3e014b915ae2
ROUNAK KUNDU <132207650+Rounakoo7@users.noreply.github.com>
2024-10-07 13:04:20 +0530

    new commit

--- frontend/src/components/Usertickets.js
+++ frontend/src/components/Usertickets.js
@@ -24,7 +24,6 @@ function Usertickets(props) {
       const response = await axios.get("/showtickets", config);
       if (response.status === 200) {
         setResponse(response.data)
-        console.log("ghgjhghj",response.data)
       }
       else {
         const errorData = await response.json()
@@ -54,31 +53,34 @@ function Usertickets(props) {
         <h1 align="center" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>My Tickets</h1>
         <br />
         <div style={{ paddingTop: "10px" }}>
-        {response.map((ticket, index) => {
-          return(
-        <div className="card mb-3 container-sm" style={{ backgroundColor: props.mode === 'dark' ? 'rgb(50 52 52)' : 'white', maxWidth: "930px", paddingTop: "10px", paddingBottom: "10px" }}>
-            <div className="row g-0">
-              <div className="col-md-4" style={{ paddingLeft: "5px", paddingTop: "15px", paddingBottom: "10px", paddingRight: "10px", borderRight: `1px solid ${props.mode === 'dark' ? 'white' : 'black'}`, color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h5 className="card-title" style={{border: `1px solid ${props.mode === 'dark' ? 'white' : 'black'}`}}><span style={{ paddingLeft: "65px" }}></span><GiTicket /> Ticket Id : {ticket.id}</h5>
-                <br />
-                <h5 className="card-title"><IoPeople /> Passengers : {ticket.passengers}</h5>
-                <h5 className="card-title"><RiMoneyRupeeCircleFill /> Fare : {ticket.fare} rupees</h5>
-                <h5 className="card-title"><RiPinDistanceFill /> Distance : {ticket.distance} {ticket.distance === 1 ? "km" : "kms"}</h5>
-              </div>
-              <div className="col-md-8" style={{ paddingTop: "5px" }}>
-                <div className="card-body" style={{ paddingLeft: "25px", paddingTop: "15px", paddingBottom: "10px", color: props.mode === 'dark' ? 'white' : 'black' }}>
-                  <h5 className="card-title"><FaTrainSubway /> Source Station : {ticket.station1}</h5>
-                  <br />
-                  <h6 className="card-title"><FaRoute /> Interchanges : {ticket.interchange_path[0] === "NULL" ? "No Interchanges" : `${ticket.interchange_path.join(" -> ")}`}</h6>
-                  <br />
-                  <h5 className="card-title"><FaTrainSubway /> Destination Station : {ticket.station2}</h5>
-                </div>
-              </div>
-              <div style={{ paddingLeft: "5px", paddingTop: "5px", color: props.mode === 'dark' ? 'white' : 'black' }}>
-                <h6 className="card-title"><BsCalendarDateFill /> Booking Time : {ticket.booking_time.replace("T"," :: ")}<span style={{ paddingLeft: "209px" }}></span><BsCalendarDateFill /> Expiry Time : {ticket.expiry_time.replace("T"," :: ")}</h6>
-              </div>
-            </div>
-          </div>)})}
+          {response.length != 0 ?
+            response.map((ticket, index) => {
+              return (
+                <div className="card mb-3 container-sm" style={{ backgroundColor: props.mode === 'dark' ? 'rgb(50 52 52)' : 'white', maxWidth: "930px", paddingTop: "10px", paddingBottom: "10px" }}>
+                  <div className="row g-0">
+                    <div className="col-md-4" style={{ paddingLeft: "5px", paddingTop: "15px", paddingBottom: "10px", paddingRight: "10px", borderRight: `1px solid ${props.mode === 'dark' ? 'white' : 'black'}`, color: props.mode === 'dark' ? 'white' : 'black' }}>
+                      <h5 className="card-title" style={{ border: `1px solid ${props.mode === 'dark' ? 'white' : 'black'}` }}><span style={{ paddingLeft: "65px" }}></span><GiTicket /> Ticket Id : {ticket.id}</h5>
+                      <br />
+                      <h5 className="card-title"><IoPeople /> Passengers : {ticket.passengers}</h5>
+                      <h5 className="card-title"><RiMoneyRupeeCircleFill /> Fare : {ticket.fare} rupees</h5>
+                      <h5 className="card-title"><RiPinDistanceFill /> Distance : {ticket.distance} {ticket.distance === 1 ? "km" : "kms"}</h5>
+                    </div>
+                    <div className="col-md-8" style={{ paddingTop: "5px" }}>
+                      <div className="card-body" style={{ paddingLeft: "25px", paddingTop: "15px", paddingBottom: "10px", color: props.mode === 'dark' ? 'white' : 'black' }}>
+                        <h5 className="card-title"><FaTrainSubway /> Source Station : {ticket.station1}</h5>
+                        <br />
+                        <h6 className="card-title"><FaRoute /> Interchanges : {ticket.interchange_path[0] === "NULL" ? "No Interchanges" : `${ticket.interchange_path.join(" -> ")}`}</h6>
+                        <br />
+                        <h5 className="card-title"><FaTrainSubway /> Destination Station : {ticket.station2}</h5>
+                      </div>
+                    </div>
+                    <div style={{ paddingLeft: "5px", paddingTop: "5px", color: props.mode === 'dark' ? 'white' : 'black' }}>
+                      <h6 className="card-title"><BsCalendarDateFill /> Booking Time : {ticket.booking_time.replace("T", " :: ")}<span style={{ paddingLeft: "209px" }}></span><BsCalendarDateFill /> Expiry Time : {ticket.expiry_time.replace("T", " :: ")}</h6>
+                    </div>
+                  </div>
+                </div>)
+            }) : <h2 align="center" style={{ color: props.mode === 'dark' ? 'white' : 'black' }} >No Active Tickets</h2>
+          }
           <br />
         </div>
       </div>
